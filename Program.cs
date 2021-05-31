956164f6b970bb9574438076b8d7bff4336822b7
Bruce Phillips <bphillip@stfm.org>
2021-05-31 14:05:49 -0500

    Added Run method

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 
 namespace dotnet_console_example
 {
+    /// <summary>
+    /// Program to get user name from command line and display a Hello {name}
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -14,6 +17,19 @@ namespace dotnet_console_example
 
             string name = args[0];
 
+            Program program = new Program();
+
+            program.Run(name);
+
+           
+        }
+
+        /// <summary>
+        /// Display Hello {name}
+        /// </summary>
+        /// <param name="name">User name</param>
+        void Run(string name)
+        {
             Console.WriteLine("Hello {0}", name);
         }
     }
