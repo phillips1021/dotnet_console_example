e233f24e84cbc98fdf578c0b427d3f56dd324f93
Bruce Phillips <bphillip@stfm.org>
2021-05-31 13:59:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dotnet_console_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("You must provide the name value as the first argument.");
+                Environment.Exit(1);
+            }
+
+            string name = args[0];
+
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
